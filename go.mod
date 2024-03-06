39176c0946a4ae3620c90f84b314e5176421d1a5
Vishal Singh <vishalsinghsrm@outlook.com>
2024-03-06 15:26:09 +0530

    feat: added config

--- go.mod
+++ go.mod
@@ -20,6 +20,7 @@ require (
 	github.com/aws/smithy-go v1.20.0 // indirect
 	github.com/beorn7/perks v1.0.1 // indirect
 	github.com/cespare/xxhash/v2 v2.2.0 // indirect
+	github.com/ghodss/yaml v1.0.0 // indirect
 	github.com/jmespath/go-jmespath v0.4.0 // indirect
 	github.com/matttproud/golang_protobuf_extensions/v2 v2.0.0 // indirect
 	github.com/prometheus/client_golang v1.18.0 // indirect
@@ -28,4 +29,5 @@ require (
 	github.com/prometheus/procfs v0.12.0 // indirect
 	golang.org/x/sys v0.15.0 // indirect
 	google.golang.org/protobuf v1.31.0 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
--- main.go
+++ main.go
@@ -2,87 +2,176 @@ package main
 
 import (
 	"context"
+	"flag"
 	"log"
+	"net/http"
+	"os"
+	"strconv"
+	"strings"
 	"time"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/cloudwatchlogs"
 	"github.com/aws/aws-sdk-go-v2/service/cloudwatchlogs/types"
+	"github.com/ghodss/yaml"
 	"github.com/prometheus/client_golang/prometheus"
-	"github.com/prometheus/client_golang/prometheus/push"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"github.com/prometheus/common/version"
 )
 
-var (
-	jobName      = "cloudwatchlogs"
-	instance     = "amazon"
-	cwLogGroup   = "apiconnect-staging_asc_api"
-	pushGateway  = "http://localhost:9091"
-	customMetric = "api_success_rate"
-)
+var yamlConfig Config
 
-var yourCustomMetric *prometheus.GaugeVec
+const collector = "cloudwatch_log_export"
 
-func init() {
-	yourCustomMetric = prometheus.NewGaugeVec(
-		prometheus.GaugeOpts{
-			Name: customMetric,
-			Help: "Your custom metric description",
-		},
-		[]string{"error_code", "url", "shop_id", "home_shop_id"},
-	)
+var lastEndTimeFile = "last_end_time.txt"
+
+type Config struct {
+	Group     string
+	Query     string
+	Metrics map[string]struct {
+		Type        string
+		Description string
+		Labels      []string
+		Value       string
+		metricDesc  *prometheus.Desc
+	}
 }
 
-func fetchAndProcessLogs(ctx context.Context, client *cloudwatchlogs.Client) {
+type QueryCollector struct {}
+
+func (e *QueryCollector) Describe(ch chan<- *prometheus.Desc) {
+	for metricName, metric := range yamlConfig.Metrics {
+		metric.metricDesc = prometheus.NewDesc(
+			prometheus.BuildFQName(collector, "", metricName),
+			metric.Description,
+			metric.Labels, nil,
+		)
+		yamlConfig.Metrics[metricName] = metric
+		log.Printf("metric description for \"%s\" registered", metricName)
+	}
+}
+
+func (e *QueryCollector) Collect(ch chan<- prometheus.Metric) {
+
+	cfg, awsError := config.LoadDefaultConfig(context.TODO(),
+		config.WithRegion("ap-south-1"), // Replace with your AWS region
+		config.WithSharedConfigProfile("amazon"),
+	)
+	if awsError != nil {
+		log.Fatalf("Error loading AWS configuration: %v", awsError)
+	}
+
+	client := cloudwatchlogs.NewFromConfig(cfg)
+
+
+	logMetric := fetchAndProcessLogs(context.TODO(), client)
+
+	log.Printf("%d",len(logMetric))
+
+	for index, lMetric := range logMetric {
+		for name, metric := range yamlConfig.Metrics {
+			// Metric labels
+			labelValues := []string{}
+			
+			for _, label := range metric.Labels {
+				labelValues = append(labelValues, lMetric[label])
+			}
+
+			// Add metric
+			switch strings.ToLower(metric.Type) {
+			case "counter":
+				ch <- prometheus.MustNewConstMetric(metric.metricDesc, prometheus.CounterValue, float64(index), labelValues...)
+			case "gauge":
+				ch <- prometheus.MustNewConstMetric(metric.metricDesc, prometheus.GaugeValue, float64(index), labelValues...)
+			default:
+				log.Panicf("Fail to add metric for %s: %s is not valid type", name, metric.Type)
+				continue
+			}
+		}
+	}
+}
+
+
+func readLastEndTimeFromFile() int64 {
+	content, err := os.ReadFile(lastEndTimeFile)
+	if err != nil {
+		log.Printf("Error reading lastEndTime file: %v", err)
+		return 0
+	}
+
+	lastEndTime, err := strconv.ParseInt(strings.TrimSpace(string(content)), 10, 64)
+	if err != nil {
+		log.Printf("Error parsing lastEndTime from file: %v", err)
+		return 0
+	}
+
+	return lastEndTime
+}
+
+func writeLastEndTimeToFile(lastEndTime int64) {
+	err := os.WriteFile(lastEndTimeFile, []byte(strconv.FormatInt(lastEndTime, 10)), 0644)
+	if err != nil {
+		log.Printf("Error writing lastEndTime to file: %v", err)
+	}
+}
+
+
+func fetchAndProcessLogs(ctx context.Context, client *cloudwatchlogs.Client) ([]map[string]string) {
+	currentTime := time.Now()
+	startTime := aws.Int64((currentTime.UnixMilli() / 1000) - 86400)
+	endTime := aws.Int64(currentTime.UnixMilli() / 1000)
+
+	lastEndTime := readLastEndTimeFromFile()
+
+	// If lastEndTime is set, use it as the new StartTime
+	if lastEndTime != 0 {
+		startTime = aws.Int64(lastEndTime)
+	}
 	startQueryInput := &cloudwatchlogs.StartQueryInput{
-		LogGroupName: &cwLogGroup,
-		StartTime:    aws.Int64((time.Now().UnixNano() / int64(time.Millisecond)) - (5 * 60 * 60 * 1000)), // 5 minutes ago
-		EndTime:      aws.Int64(time.Now().UnixNano() / int64(time.Millisecond)),
-		// QueryString:  aws.String(`fields @timestamp, message | filter message like /error_code/ and message like /_url/ | parse message "=> error_code * => {\"_url\":\"\/*\",\"shop_id\":\"*\",\"home_shop_id\":\"*\"" as error_code, url, shop_id, home_shop_id`),
-		QueryString: aws.String(`fields @timestamp, message`),
+		LogGroupName: aws.String(yamlConfig.Group),
+		StartTime:    startTime, // 12 hours ago
+		EndTime:      endTime,
+		QueryString:  aws.String(yamlConfig.Query),
 	}
 
 	startQueryOutput, err := client.StartQuery(ctx, startQueryInput)
+	resultSlice := make([]map[string]string, 0)
 	if err != nil {
 		log.Printf("Error starting CloudWatch Logs query: %v", err)
-		return
+		return resultSlice
 	}
 
 	queryID := startQueryOutput.QueryId
 
-	for {
 		getQueryResultsInput := &cloudwatchlogs.GetQueryResultsInput{
 			QueryId: queryID,
 		}
 
-		resultsResp, err := client.GetQueryResults(ctx, getQueryResultsInput)
-		if err != nil {
-			log.Printf("Error getting CloudWatch Logs query results: %v", err)
-			return
+		for {
+			resultsResp, err := client.GetQueryResults(ctx, getQueryResultsInput)
+			if err != nil {
+				log.Printf("Error getting CloudWatch Logs query results: %v", err)
+				return resultSlice;
+			}
+
+			log.Println(resultsResp.Status);
+
+
+			if resultsResp.Status == types.QueryStatusComplete {
+				// The query has completed, exit the loop
+				for _, result := range resultsResp.Results {
+					// Convert result to a map[string]string
+					resultMap := convertResultToMap(result)
+					resultSlice = append(resultSlice, resultMap)
+				}
+				writeLastEndTimeToFile(currentTime.UnixMilli() / 1000)
+				break
+			}
+			time.Sleep(2 * time.Second)
 		}
 
-		for _, result := range resultsResp.Results {
-			// Convert result to a map[string]string
-			resultMap := convertResultToMap(result)
-
-			// Extract values based on your log format
-			message := getStringValue(resultMap, "message");
-			log.Print(message);
-			errorCode := getStringValue(resultMap, "error_code")
-			url := getStringValue(resultMap, "url")
-			shopID := getStringValue(resultMap, "shop_id")
-			homeShopID := getStringValue(resultMap, "home_shop_id")
-	
-			// Process your values (e.g., push to Prometheus)
-			processValues(errorCode, url, shopID, homeShopID)
-		}
-
-		if resultsResp.Status == types.QueryStatusComplete {
-			break
-		}
-
-		time.Sleep(2 * time.Second) // Sleep before checking results again
-	}
+	return resultSlice;
 }
 
 // Helper function to convert ResultField to a map[string]string
@@ -96,50 +185,48 @@ func convertResultToMap(result []types.ResultField) map[string]string {
     return resultMap
 }
 
-func getStringValue(result map[string]string, key string) string {
-	if value, ok := result[key]; ok {
-		return value
-	}
-	return ""
-}
-
-func processValues(errorCode, url, shopID, homeShopID string) {
-	// Process your values (e.g., push to Prometheus)
-	// In this example, pushing to Prometheus is shown
-	yourCustomMetric.WithLabelValues(errorCode, url, shopID, homeShopID).Set(1)
-}
-
-func pushMetrics() {
-	reg := prometheus.NewPedanticRegistry()
-	reg.MustRegister(yourCustomMetric)
-
-	gatherers := prometheus.Gatherers{reg}
-
-	pusher := push.New(pushGateway, jobName).
-		Gatherer(gatherers).
-		Grouping("instance", instance)
-
-	err := pusher.Add()
-	if err != nil {
-		log.Printf("Error pushing metrics: %v", err)
-	}
-}
 
 func main() {
-	cfg, err := config.LoadDefaultConfig(context.TODO(),
-        config.WithRegion("ap-south-1"), // Replace with your AWS region
-        config.WithSharedConfigProfile("amazon"),
-    )
-	log.Printf(cfg.AppID);
-	if err != nil {
-		log.Fatalf("Error loading AWS configuration: %v", err)
+	
+	var err error
+	var configFile, bind string
+	
+	flag.StringVar(&configFile, "config", "config.yml", "configuration file")
+	flag.StringVar(&bind, "bind", "0.0.0.0:9104", "bind")
+	flag.Parse()
+
+	// =====================
+	// Load config & yaml
+	// =====================
+	var b []byte
+	if b, err = os.ReadFile(configFile); err != nil {
+		log.Fatalf("Failed to read config file: %s", err)
+		os.Exit(1)
 	}
 
-	client := cloudwatchlogs.NewFromConfig(cfg)
-
-	// Fetch and process logs every 1 minute
-	for range time.Tick(1 * time.Minute) {
-		fetchAndProcessLogs(context.TODO(), client)
-		pushMetrics()
+	// Load yaml
+	if err := yaml.Unmarshal(b, &yamlConfig); err != nil {
+		log.Fatalf("Failed to load config: %s", err)
+		os.Exit(1)
 	}
+
+	log.Printf("Register version collector - %s", collector)
+	prometheus.Register(version.NewCollector(collector))
+	prometheus.Register(&QueryCollector{})
+
+	// Register http handler
+	log.Printf("HTTP handler path - %s", "/metrics")
+	http.HandleFunc("/metrics", func(w http.ResponseWriter, r *http.Request) {
+		h := promhttp.HandlerFor(prometheus.Gatherers{
+			prometheus.DefaultGatherer,
+		}, promhttp.HandlerOpts{})
+		h.ServeHTTP(w, r)
+	})
+
+	// start server
+	log.Printf("Starting http server - %s", bind)
+	if err := http.ListenAndServe(bind, nil); err != nil {
+		log.Panicf("Failed to start http server: %s", err)
+	}
+
 }
